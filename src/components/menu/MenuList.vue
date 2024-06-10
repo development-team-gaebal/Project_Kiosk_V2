35b15e2b0f9325480fca9d651f7fc2e6a04ae66d
phygloria <phygloria@gmail.com>
2024-06-10 10:44:28 +0900

    menu

---
+++ src/components/menu/MenuList.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { reactive } from "vue";
+import { scholar } from "../menu/MenuData.js";
+
+const MenuData = reactive(scholar);
+</script>
+
+<template>
+  <div class="MenuList">
+    <div v-for="item in MenuData" :key="item.id" class="menu-item">
+      <img class="image" :src="item.img" :alt="item.name" />
+      <p>{{ item.name }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.MenuList {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.menu-item {
+  margin: 10px;
+  text-align: center;
+}
+
+.image {
+  width: 200px;
+  height: auto;
+}
+</style>
